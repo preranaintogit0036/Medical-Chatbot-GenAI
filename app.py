7ba8b6df6d87eb0c1eecb5cefd1bf2db1b2294d2
Prerana Singh <120215815+preranaintogit0036@users.noreply.github.com>
2025-02-28 23:44:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from pathlib import Path
 import google.generativeai as genai
 
-from Medical-Chatbot-GenAI.api_key import api_key  # Ensure api_key.py is correctly set up
+from Medical_Chatbot_GenAI.api_key import api_key  # Ensure api_key.py is correctly set up
 
 # Set Streamlit page config (MUST be the first command)
 st.set_page_config(page_title="SwastiCare", page_icon="🩺")
